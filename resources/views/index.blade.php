805cb9544889aad62d88d3fe43391d5cab0828e2
Yeisonryhn <yeisonf15@gmail.com>
2018-10-24 02:21:00 -0430

    Pruebas css

---
+++ resources/views/index.blade.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Sistema de Peritaje</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    
+    <body>
+
+        <header>
+            <div id="logo">Sistema de Peritaje<div>   
+            
+            <div class="usuario" id="ingreso"><a href="">Ingreso</a></div>
+            <div class="usuario" id="registro"><a href="">Registro</a></div>           
+            
+        </header><!-- /header -->
+
+        <aside>  
+            <div id="imagen">
+                <img src="img/peritaje.jpg" width="900" height="600">
+            </div> 
+            <div id="articulo">
+                <article> 
+                    <p><h1>Peritaje de Vehiculos.</h1>
+                    <br>Peritaje de vehículos ¿en qué consiste? Objetivo del peritaje de vehículos y forma de desarrollo
+                    Cuando se habla de peritaciones el peritaje de vehículos es uno de los más conocidos. La intervención del perito experto en vehículos puede darse dentro de un proceso judicial, pero lo más común es que se produzca fuera del mismo, dentro de las actividades que son propias de los seguros de vehículos.
+
+                    El perito en vehículos es un profesional en la materia. Habitualmente suele ser una persona con un Grado Formativo Superior (FP II) en automoción, aunque también puede llegar a operar como perito en estas cuestiones un licenciado o graduado en ingeniería u profesiones homólogas.
+
+                    Hacer una peritación no es sencillo, por lo que lo normal es que los profesionales que quieren dedicarse al peritaje de vehículos y tienen la titulación necesaria, realicen algún tipo de curso de peritaje de vehículos.</p>                
+                </article>
+                    
+            </div>    
+            
+        </aside>
+        <footer class="ancho">   
+            <div class="ancho" id="linea"></div>
+            <dir id="copy">&copy Todos los derechos reservados.</dir>
+        </footer>
+    </body>
+</html>
+
+<!-- Styles
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style> -->
