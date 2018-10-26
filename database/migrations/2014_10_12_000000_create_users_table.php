c7c96f908b4a1c564554a55c3ebdafb969fb48ee
Yeisonryhn <yeisonf15@gmail.com>
2018-10-26 02:52:08 -0430

    Introduccion a Migraciones y solucion error 1071 al migrar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        //esta funcion es la que crea la tabla de usuarios
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
@@ -20,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+        //en caso de error 1071 Para las personas que lleguen a tener el error de "Syntax error or access violation: 1071 Specified key was too long...." una forma de solucionarlo es yendo al archivo "AppServiceProvider.php" ubicado en "mi-proyecto/app/providers/AppServiceProvider.php" lo que tienen que hacer es incluir al principio del archivo "use Illuminate\Support\Facades\Schema;" y en la funcion llamada "boot" incluir "Schema::defaultStringLength(191);".  Al final guardan el archivo y vuelven a ejecutar el comando  php artisan migrate﻿
         });
     }
 
@@ -30,6 +33,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        //esta funcion elimina la tabla de usuarios
+
         Schema::dropIfExists('users');
     }
 }
